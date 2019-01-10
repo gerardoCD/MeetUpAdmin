ce70c6325878d56b00a6e5da790edbf2d39cc8ab
Abraham Quezada <abrahamqt@gmail.com>
2019-01-10 13:25:06 -0600

    LaunchScreen and first views

--- Meet Up Admin/Meet Up Admin/AppDelegate.swift
+++ Meet Up Admin/Meet Up Admin/AppDelegate.swift
@@ -12,12 +12,74 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let blueSplash = UIColor(red: 0x09, green: 0x09, blue: 0x3B, alpha: 1)
+    let screen = UIScreen.main.bounds
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.splashScreen()
         return true
     }
+    
+    //LaunchScreen
+    private func splashScreen(){
+        let launchScreenVC = UIStoryboard.init(name: "LaunchScreen", bundle: nil)
+        let rootVC = launchScreenVC.instantiateViewController(withIdentifier: "splashController")
+        self.window?.rootViewController = rootVC
+        self.window?.makeKeyAndVisible()
+        Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(dismissSplashController), userInfo: nil, repeats: false)
+        
+        // logo mask
+        rootVC.view.layer.mask = CALayer()
+        rootVC.view.layer.mask?.backgroundColor = blueSplash.cgColor
+        let screenWidth = screen.size.width
+        let screenHeight = screen.size.height
+        rootVC.view.layer.mask?.bounds = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
+        rootVC.view.layer.mask?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        rootVC.view.layer.mask?.position = CGPoint(x: rootVC.view.frame.width / 2, y: rootVC.view.frame.height / 2)
+        
+        // logo mask background view
+        let maskBgView = UIView(frame: rootVC.view.frame)
+        rootVC.view.addSubview(maskBgView)
+        rootVC.view.bringSubviewToFront(maskBgView)
+        // logo mask background view animation
+        UIView.animate(withDuration: 1.5,
+                       delay: 0.35,
+                       options: UIView.AnimationOptions.curveEaseOut,
+                       animations: {
+                        maskBgView.alpha = 0.5
+        },
+                       completion: { finished in
+                        maskBgView.removeFromSuperview()
+        })
+        
+        // root view animation
+        UIView.animate(withDuration: 2.5,
+                       delay: 1.0,
+                       options: [],
+                       animations: {
+                        self.window!.rootViewController!.view.transform = CGAffineTransform(scaleX: 1.15, y: 1.15)
+        },
+                       completion: { finished in
+                        UIView.animate(withDuration: 0.3,
+                                       delay: 0.0,
+                                       options: UIView.AnimationOptions.curveEaseInOut,
+                                       animations: {
+                                        self.window!.rootViewController!.view.transform = .identity
+                        },
+                                       completion: nil)
+        })
+        
+    }
+    
+    @objc func dismissSplashController(){
+        let mainVC = UIStoryboard.init(name: "Main", bundle: nil)
+        let rootVC = mainVC.instantiateViewController(withIdentifier: "initController")
+        self.window?.rootViewController = rootVC
+        self.window?.makeKeyAndVisible()
+    }
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
