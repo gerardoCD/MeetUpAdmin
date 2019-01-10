7a411fa687d44cadcfdb838e3eab756989e607e8
Abraham Quezada <abrahamqt@gmail.com>
2019-01-10 13:41:26 -0600

    main view

---
+++ Meet Up Admin/Meet Up Admin/Controllers/CreateViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CreateViewController.swift
+//  Meet Up Admin
+//
+//  Created by Abraham Quezada on 1/10/19.
+//  Copyright © 2019 Gerardo Castillo Duran . All rights reserved.
+//
+
+import UIKit
+
+class CreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
