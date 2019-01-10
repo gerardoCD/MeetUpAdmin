d4fe6b21c6c9b14307191f14565ac69300d82f8b
Gerardo <ferrari_gerardo@live.com.mx>
2019-01-09 19:08:25 -0600

    Project Empty

---
+++ Meet Up Admin/Meet Up Admin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meet Up Admin
+//
+//  Created by Gerardo Castillo Duran  on 1/9/19.
+//  Copyright © 2019 Gerardo Castillo Duran . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Meet Up Admin/Meet Up AdminTests/Meet_Up_AdminTests.swift
@@ -0,0 +1,34 @@
+//
+//  Meet_Up_AdminTests.swift
+//  Meet Up AdminTests
+//
+//  Created by Gerardo Castillo Duran  on 1/9/19.
+//  Copyright © 2019 Gerardo Castillo Duran . All rights reserved.
+//
+
+import XCTest
+@testable import Meet_Up_Admin
+
+class Meet_Up_AdminTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
