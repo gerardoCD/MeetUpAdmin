aa288c242986ed49800b1e1ca42138d472ece1ff
Abraham Quezada <abrahamqt@gmail.com>
2019-01-12 23:49:25 -0600

    LoginView and SignUp View

---
+++ Meet Up Admin/Meet Up Admin/Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Meet Up Admin
+//
+//  Created by Abraham Quezada on 1/12/19.
+//  Copyright © 2019 Gerardo Castillo Duran . All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Meet Up Admin/Meet Up Admin/Controllers/SignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpViewController.swift
+//  Meet Up Admin
+//
+//  Created by Abraham Quezada on 1/12/19.
+//  Copyright © 2019 Gerardo Castillo Duran . All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
