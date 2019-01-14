186d23e907e8d71da0cd6b273248fdb43fdfce6a
Gerardo <ferrari_gerardo@live.com.mx>
2019-01-14 14:12:03 -0600

    Firebase DataBase

--- Meet Up Admin/Meet Up Admin/Controllers/MainViewController.swift
+++ Meet Up Admin/Meet Up Admin/Controllers/MainViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import Firebase
+import FirebaseAuth
 
 
 class MainViewController: UIViewController {
--- Meet Up Admin/Meet Up Admin/Controllers/SignUpViewController.swift
+++ Meet Up Admin/Meet Up Admin/Controllers/SignUpViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class SignUpViewController: UIViewController {
     
     
     @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtConfirmPassword: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,5 +23,100 @@ class SignUpViewController: UIViewController {
         signUpButton.layer.cornerRadius = 8.0
         signUpButton.layer.masksToBounds = true
     }
+    
+    @IBAction func SignUP(_ sender: UIButton) {
+        
+        if txtPassword.text == txtConfirmPassword.text{
+            Auth.auth().createUser(withEmail: txtEmail.text!, password: txtPassword.text!) { (authResult, error) in
+                if error == nil {
+                    let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                    changeRequest?.displayName = self.txtName.text
+                    changeRequest?.commitChanges { error in
+                        if error == nil {
+                            //Alert
+                            saveProfile(userName: self.lblUserName.text!,userEmail:self.lblEmail.text!) { success in
+                                if success {
+                                    let alert = UIAlertController(title: "Congratulations", message: "You are Singed", preferredStyle: .alert)
+                                    alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: {(alert: UIAlertAction!) in  self.navigationController?.popToRootViewController(animated: true)}))
+                                    self.present(alert, animated: true)
+                                    
+                                }
+                                
+                            }
+                        } else {
+                            let alert = UIAlertController(title: "Error", message: "Your datas are incorrects", preferredStyle: .alert )
+                            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler:nil))
+                            self.present(alert, animated: true)
+                        }
+                    }
+                } else {
+                    if let errCode = AuthErrorCode(rawValue: error!._code) {
+                        switch errCode {
+                        case .emailAlreadyInUse:
+                            let alert = UIAlertController(title: "Error", message: "Your email is In Use", preferredStyle: .alert )
+                            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler:nil))
+                            self.present(alert, animated: true)
+                        case .invalidEmail:
+                            let alert = UIAlertController(title: "Error", message: "Your format email is incorrect", preferredStyle: .alert )
+                            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler:nil))
+                            self.present(alert, animated: true)
+                        case .userDisabled:
+                            let alert = UIAlertController(title: "Error", message: "Your user has been disable \n Try contact us", preferredStyle: .alert )
+                            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler:nil))
+                            self.present(alert, animated: true)
+                        case .wrongPassword:
+                            let alert = UIAlertController(title: "Error", message: "Your datas are incorrects", preferredStyle: .alert )
+                            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler:nil))
+                            self.present(alert, animated: true)
+                        case .weakPassword:
+                            let alert = UIAlertController(title: "Error", message: "Your datas are incorrects", preferredStyle: .alert )
+                            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler:nil))
+                            self.present(alert, animated: true)
+                        case .userNotFound:
+                            let alert = UIAlertController(title: "Error", message: "Your datas are incorrects", preferredStyle: .alert )
+                            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler:nil))
+                            self.present(alert, animated: true)
+                        case .networkError:
+                            let alert = UIAlertController(title: "Error", message: "You are not connect on Internet", preferredStyle: .alert )
+                            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler:nil))
+                            self.present(alert, animated: true)
+                        default:
+                            let alert = UIAlertController(title: "Error", message: "You have a error \n Try Try contact us", preferredStyle: .alert )
+                            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler:nil))
+                            self.present(alert, animated: true)
+                        }
+                    }
+                    
+                }
+                
+                
+                
+            }
+            
+        }else{
+            let alert = UIAlertController(title: "Error", message: "Your passwords are not equals", preferredStyle: .alert )
+            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler:nil))
+            self.present(alert, animated: true)
+        }
+        
+    }
+    
+    
+    func saveProfile(userName:String,userEmail:String, completion: @escaping ((_ success:Bool)->())) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        let databaseRef = Database.database().reference().child("users/profile/\(uid)")
+        
+        let userObject = [
+            "email": userEmail,
+            "username": userName,
+            ] as [String:Any]
+        
+        databaseRef.setValue(userObject) { error, ref in
+            completion(error == nil)
+        }
+    }
+    
+    
 
 }
