00dcc077384d48cbcbb801e876718a04aec735ce
Gerardo <ferrari_gerardo@live.com.mx>
2019-01-14 17:47:58 -0600

    Firebase Storage Done

--- Meet Up Admin/Meet Up Admin/Controllers/CreateViewController.swift
+++ Meet Up Admin/Meet Up Admin/Controllers/CreateViewController.swift
@@ -7,11 +7,25 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class CreateViewController: UIViewController {
     
     
     @IBOutlet weak var createButton: UIButton!
+    @IBOutlet weak var txtEventTitle: UITextField!
+    @IBOutlet weak var imgEvent: UIImageView!
+    @IBOutlet weak var txtStartDate: UITextField!
+    @IBOutlet weak var txtEndDate: UITextField!
+    @IBOutlet weak var txtCountry: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtStreet: UITextField!
+    @IBOutlet weak var txtPrice: UITextField!
+    @IBOutlet weak var txtAvailableTickets: UITextField!
+    @IBOutlet weak var txtStartTime: UITextField!
+    @IBOutlet weak var txtEndTime: UITextField!
+    @IBOutlet weak var txtDescription: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,5 +33,28 @@ class CreateViewController: UIViewController {
         createButton.layer.cornerRadius = 8.0
         createButton.layer.masksToBounds = true
         }
+    
+    
+    @IBAction func CreateEvent(_ sender: UIButton) {
+      //  if Auth.auth().currentUser != nil {
+            Event.saveEvent(City: "s", Country: "a", Description: "a", EndDate: "a", EndHour: "2", ImageUrl: "2ww1", Place: "ss", Name: "dwed", Price: "wzw", StartDate: "wwxw", StartHour: "ededed", Street: "ededed") { (succes) in
+                if succes {
+                    print("Yeaaaaaa")
+                }else{
+                    print("Error")
+                }
+            }
+            
+       // }else{
+            
+            performSegue(withIdentifier: "eventToLog", sender: nil)
+            
+        //}
+    }
+    
+    
+
+    
+    
     
 }
---
+++ Meet Up Admin/Meet Up Admin/Models/Ticket.swift
@@ -0,0 +1,9 @@
+//
+//  Ticket.swift
+//  Meet Up Admin
+//
+//  Created by Gerardo Castillo Duran  on 1/14/19.
+//  Copyright © 2019 Gerardo Castillo Duran . All rights reserved.
+//
+
+import Foundation
