9729f51df32127c2454e3cfbbeabe3031e3e6371
Gerardo <ferrari_gerardo@live.com.mx>
2019-01-15 17:39:20 -0600

    Uploading Images from Image Gallery and Camera
    This is important, Upload image 4.2 Swift

--- Meet Up Admin/Meet Up Admin/Controllers/CreateViewController.swift
+++ Meet Up Admin/Meet Up Admin/Controllers/CreateViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import FirebaseAuth
 
-class CreateViewController: UIViewController {
+class CreateViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     
     @IBOutlet weak var createButton: UIButton!
@@ -25,6 +25,12 @@ class CreateViewController: UIViewController {
     @IBOutlet weak var txtStartTime: UITextField!
     @IBOutlet weak var txtEndTime: UITextField!
     @IBOutlet weak var txtDescription: UITextField!
+    
+    var picker = UIImagePickerController();
+    var alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+    var viewController: UIViewController?
+    var pickImageCallback : ((UIImage) -> ())?;
+
     
     
     override func viewDidLoad() {
@@ -175,6 +181,79 @@ class CreateViewController: UIViewController {
             
         }
     }
+    
+    
+    @IBAction func uploadImage(_ sender: UITapGestureRecognizer) {
+        
+        CreateViewController().pickImage(self){ image in
+            self.imgEvent.image = image
+        }
+    }
+    
+    
+    
+    func pickImage(_ viewController: UIViewController, _ callback: @escaping ((UIImage) -> ())) {
+        pickImageCallback = callback;
+        self.viewController = viewController;
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default){
+            UIAlertAction in
+            self.openCamera()
+        }
+        let gallaryAction = UIAlertAction(title: "Gallary", style: .default){
+            UIAlertAction in
+            self.openGallery()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel){
+            UIAlertAction in
+        }
+        
+        // Add the actions
+        picker.delegate = self
+        alert.addAction(cameraAction)
+        alert.addAction(gallaryAction)
+        alert.addAction(cancelAction)
+        alert.popoverPresentationController?.sourceView = self.viewController!.view
+        viewController.present(alert, animated: true, completion: nil)
+    }
+    
+    func openCamera(){
+        alert.dismiss(animated: true, completion: nil)
+        if(UIImagePickerController .isSourceTypeAvailable(.camera)){
+            picker.sourceType = .camera
+            self.viewController!.present(picker, animated: true, completion: nil)
+        } else {
+            
+            let alertWarning = UIAlertController(title:"Warning" , message: "You don't have camera", preferredStyle: .alert)
+            self.present(alertWarning, animated: true)
+        }
+    }
+    func openGallery(){
+        alert.dismiss(animated: true, completion: nil)
+        picker.sourceType = .photoLibrary
+        self.viewController!.present(picker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    // For Swift 4.2
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        pickImageCallback?(image)
+    }
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, pickedImage: UIImage?) {
+    }
+
+    
+    
     
     
 
